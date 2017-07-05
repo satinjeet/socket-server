2f6ef9dacf31e131d57b517b6de0f8967f3fd2ec
Satin <satin.jeet@gmail.com>
2017-07-05 09:54:22 -0400

    connect player

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+var players = {};
+
 io.on('connection', function(socket){
     console.log('a user connected');
 
@@ -9,6 +12,14 @@ io.on('connection', function(socket){
         console.log('pinged', data);
         socket.send('ping:accepted');
     });
+
+    socket.on('player::connected', function(player) {
+        console.log('Player connected: ', arguments);
+        players[player.name] = player;
+
+        console.log(players);
+        socket.broadcast.emit('player::connected::update', player);
+    })
 
 });
 
